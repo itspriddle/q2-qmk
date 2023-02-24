0e684fbd31a1fd24eb6cb9c02cf64b6c0e942b02
Joshua Priddle <jpriddle@me.com>
2023-02-24 18:35:02 -0500

    let's try pressing the button to max brightness

--- keyboards/keychron/q2/ansi_encoder/keymaps/josh/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/josh/keymap.c
@@ -25,6 +25,15 @@ enum layers{
     _FN3
 };
 
+// Custom keykodes
+enum my_keycodes {
+#if defined(VIA_ENABLE)
+  KC_BRI_MAX = USER00
+#else
+  KC_BRI_MAX = SAFE_RANGE
+#endif
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_67(
         KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
@@ -55,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______, _______, _______),
 
     [_FN3] = LAYOUT_ansi_67(
-        KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          _______,
+        KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          KC_BRI_MAX,
         RGB_TOG, RGB_MOD,  RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,  _______,  _______,          _______,
         _______, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______,  _______,            _______,          _______,
         _______,           _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______, _______,
@@ -126,11 +135,26 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 // }
 #endif
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // https://github.com/qmk/qmk_firmware/blob/master/keyboards/keychron/common/keychron_common.c#L42
-    if (!process_record_keychron(keycode, record)) {
-        return false;
-    }
+uint8_t mod_state;
 
-    return true;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // https://github.com/qmk/qmk_firmware/blob/master/keyboards/keychron/common/keychron_common.c#L42
+  if (!process_record_keychron(keycode, record)) {
+    return false;
+  }
+
+  switch (keycode) {
+    case KC_BRI_MAX:
+      if (record->event.pressed) {
+        clear_mods();
+        for (int i = 0; i < 100) {
+          tap_code16(LCTL(KC_BRIU));
+        }
+      } else {
+        set_mods(mod_state);
+      }
+      break;
+  }
+
+  return true;
 }
